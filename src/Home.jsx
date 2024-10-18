ccf49e2c58b9a7ffb60c34372b9df8fc50fc3fe1
Jason <jason5donoghue@gmail.com>
2024-10-18 15:02:28 +0100

    home page now displaying

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,118 +1,22 @@
-import React, { useRef, useEffect } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import Feature from "ol/Feature";
-import Point from "ol/geom/Point";
-import VectorSource from "ol/source/Vector";
-import VectorLayer from "ol/layer/Vector";
-import { fromLonLat } from "ol/proj";
-import Style from "ol/style/Style";
-import Icon from "ol/style/Icon";
-import "ol/ol.css";
-import { storedData } from "./data";
-import Pumpkin from "./Pumpkin.svg";
-import Target from "./Target.svg";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const OLMap = () => {
-  const mapElement = useRef();
+const Home = () => {
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const map = new Map({
-      target: mapElement.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([
-          storedData[0].address[0],
-          storedData[0].address[1],
-        ]),
-        zoom: 16,
-      }),
-    });
-
-    const iconStyle = new Style({
-      image: new Icon({
-        anchor: [0.5, 1],
-        src: Pumpkin,
-        scale: 0.2,
-      }),
-    });
-
-    const iconStyle2 = new Style({
-      image: new Icon({
-        anchor: [0.5, 1],
-        src: Target,
-        scale: 0.3,
-      }),
-    });
-
-    let targetIconCount = 0;
-
-    const features = storedData.map((data, index) => {
-      const marker = new Feature({
-        id: `marker${index}`,
-        data: data,
-        geometry: new Point(fromLonLat([data.address[0], data.address[1]])),
-      });
-      if (targetIconCount < 1) {
-        marker.setStyle(iconStyle2);
-        targetIconCount++;
-      } else {
-        marker.setStyle(iconStyle);
-      }
-      return marker;
-    });
-
-    const vectorSource = new VectorSource({
-      features: features,
-    });
-
-    const vectorLayer = new VectorLayer({
-      source: vectorSource,
-    });
-
-    map.addLayer(vectorLayer);
-
-    map.on("click", function (e) {
-      map.forEachFeatureAtPixel(e.pixel, function (feature) {
-        console.log(e);
-        console.log(feature.getId());
-        console.log(feature);
-        console.log(feature.get("data"));
-      });
-    });
-
-    return () => {
-      map.setTarget(null);
-    };
-  }, []);
+  const goToMap = () => {
+    navigate('/map');
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <style>
-        {`
-          @media (max-width: 600px) {
-            .map-container {
-              width: 100%;
-              height: 300px;
-            }
-          }
-        `}
-      </style>
-      <div ref={mapElement} className="map-container" style={{ width: "100%", height: "500px" }}></div>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <button>Go to Form</button>
+      <br/>
+      <button onClick={goToMap}>Go to Map</button>
+      
     </div>
   );
 };
 
-export default OLMap;
+export default Home;
