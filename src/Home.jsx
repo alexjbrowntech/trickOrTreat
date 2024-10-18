2de118d89fc9063ce70af587272489cf51232350
Jason <jason5donoghue@gmail.com>
2024-10-18 16:33:20 +0100

    navigation to map and form from home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
-  const goToMap = () => {
-    navigate('/map');
+  const goTo = (location) => {
+    navigate(location);
   };
-
 
   return (
     <div>
       <h1>Trick or Treat Tracker</h1>
-      <h3>Worried about where to go on Halloween? Or who will come to your door? Tell your neighbourhood you're ready to welcome some trick-or-treaters here!</h3>
+      <h3>
+        Worried about where to go on Halloween? Or who will come to your door?
+        Tell your neighbourhood you're ready to welcome some trick-or-treaters
+        here!
+      </h3>
       <p>Want to add your house to the map?</p>
-      <button>Go to Form</button>
-      <br/>
+      <button onClick={() => goTo("/form")}>Go to Form</button>
+      <br />
       <p>Want to plan your route?</p>
-      <button onClick={goToMap}>Go to Map</button>
-      
+      <button onClick={() => goTo("/map")}>Go to Map</button>
     </div>
   );
 };
